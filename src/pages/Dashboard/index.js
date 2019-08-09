753c64071519f7e4d57e622dd4d9fa02bbc70590
Pedro Morissawa <pmorissawa@gmail.com>
2019-08-09 02:21:44 -0300

    fix bug sigla partido senadores dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,6 +3,7 @@ import AuthService from "../../components/Auth/auth-services";
 import { Link } from "react-router-dom";
 import CardPolitico from "../../components/CardPolitico/CardPolitico";
 import "./dashboard.css";
+import Axios from "axios";
 
 class Dashboard extends Component {
   constructor(props) {
@@ -13,15 +14,17 @@ class Dashboard extends Component {
       senFavoritos: [],
       deputadostodos: [],
       senadorestodos: [],
+      partidosSenadores: [],
     };
     this.service = new AuthService();
+    this.axios = Axios.create({})
   }
 
   componentDidMount() {
     this.service.loggedin().then(response => {
       let { firstName, depFavoritos, senFavoritos } = response;
 
-      console.log(depFavoritos, senFavoritos)
+      // console.log(depFavoritos, senFavoritos)
 
       this.setState({
         username: firstName,
@@ -73,22 +76,36 @@ class Dashboard extends Component {
                 String(senador.IdentificacaoParlamentar.CodigoParlamentar)
               )
             )
-            .map(senador => {
-              return (
-                <CardPolitico
-                  className="card-politician-horizontal"
-                  key={senador.IdentificacaoParlamentar.CodigoParlamentar}
-                  id={senador.IdentificacaoParlamentar.CodigoParlamentar}
-                  politician="/senador/"
-                  politicianName={this.titleCase(
-                    senador.IdentificacaoParlamentar.NomeParlamentar
-                  )}
-                  uf={senador.IdentificacaoParlamentar.UfParlamentar}
-                  backImage={
-                    senador.IdentificacaoParlamentar.UrlFotoParlamentar
+            .map((senador, idx) => {
+              this.axios.get(`http://legis.senado.leg.br/dadosabertos/senador/${senador.IdentificacaoParlamentar.CodigoParlamentar}`)
+                .then((sen) => {
+                  // let counter = 0;
+                  if (this.state.partidosSenadores.length < this.state.senFavoritos.length) {
+                    this.setState({
+                      partidosSenadores: [...this.state.partidosSenadores, sen.data.DetalheParlamentar.Parlamentar.FiliacaoAtual.Partido.SiglaPartido]
+                    })
+                    // counter += 1;
                   }
-                />
-              );
+                  
+                  console.log(this.state.partidosSenadores)
+                })
+                .catch((e) => console.log(e))
+                return (
+                  <CardPolitico
+                    className="card-politician-horizontal"
+                    key={senador.IdentificacaoParlamentar.CodigoParlamentar}
+                    id={senador.IdentificacaoParlamentar.CodigoParlamentar}
+                    politician="/senador/"
+                    politicianName={this.titleCase(
+                      senador.IdentificacaoParlamentar.NomeParlamentar
+                    )}
+                    uf={senador.IdentificacaoParlamentar.UfParlamentar}
+                    backImage={
+                      senador.IdentificacaoParlamentar.UrlFotoParlamentar
+                    }
+                    siglaPartido={this.state.partidosSenadores[idx]}
+                  />
+                  );
             })}
           {this.state.deputadostodos
             .filter(deputado =>
@@ -104,6 +121,7 @@ class Dashboard extends Component {
                   politicianName={this.titleCase(deputado.nomeDeputado)}
                   uf={deputado.siglaUf}
                   backImage={deputado.urlFoto}
+                  siglaPartido={deputado.sigla}
                 />
               );
             })}
